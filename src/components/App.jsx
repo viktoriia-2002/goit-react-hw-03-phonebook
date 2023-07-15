4c422f56cf3265dd5eb53e03f96ea534ad36bc15
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-07-15 12:57:16 -0400

    Created repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+import ContactForm from './ContactForm';
+import { Container } from './App.styled';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleNewContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm
+          handleNewContact={this.handleNewContact}
+          contacts={contacts}
+        />
+        <h2>Contacts</h2>
+
+        <Filter onChange={this.handleFilter} value={filter} />
+        <ContactList
+          contacts={filteredContacts}
+          handleDelete={this.handleDelete}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
